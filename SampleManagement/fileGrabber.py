e4ef13f10a1c3f08dd5da5a6bd9d8396781b90ca
sadiess <52578915+sadiess@users.noreply.github.com>
2019-07-16 13:44:25 -0500

    bug fix
    Fixed a bug where in fileGrabber would only grab the most recent of the DoubleMuon files, not the most recent of A, B, C, and D. Also updated 2018.json to reflect changes.

--- SampleManagement/fileGrabber.py
+++ SampleManagement/fileGrabber.py
@@ -32,6 +32,8 @@ beans={}
 beans['2018'] = ['/store/group/lpcmetx/MCSIDM/ffNtuple/2018']
 beans['TTJets'] = '/store/group/lpcmetx/SIDM/ffNtuple/2018' #Needed for masking later, see line 68
 
+datadataset = dict.fromkeys(['Run2018A-17Sep2018-v2', 'Run2018B-17Sep2018-v1', 'Run2018C-17Sep2018-v1', 'Run2018D-PromptReco-v2'])
+
 lumi = 60.432 #This is the luminosity of the desired period for weight scaling
 
 def split(arr, size):
@@ -58,30 +60,29 @@ for folder in beans['2018']:
         if options.dataset and options.dataset not in dataset: continue 
             
         timestampdirs = []
+        urllist = []
         
         '''
-        The Dataset 'QCD_Pt-30to50' is not available on disk. Lines 66-70 mask out this dataset, but
+        The Dataset 'QCD_Pt-30to50' is not available on disk. This section masks out this dataset, but
         can be removed if the dataset becomes available.
         '''
 
         if dataset == 'QCD_Pt-30to50_MuEnrichedPt5_TuneCP5_13TeV_pythia8':
-            print("Opening", extrafolder)
-            print("Looking into", extrafolder+"/"+dataset)
+            print("Looking into", dataset)
             print('This dataset has been masked out.')
             continue
-        
+
         '''
-        The dataset 'TTJets' has files stored in 2 locations: /MCSIDM and /SIDM. This extra section,
-        lines 80-96, checks the additional folder, then later the most recent timestamp between the two
-        folders is selected.
+        The TTJets dataset has data saved in multiple places, so this checks both of them and returns the
+        most recent timestamp set only.
         '''
 
-        if dataset == 'TTJets_TuneCP5_13TeV-madgraphMLM-pythia8':
+        elif dataset == 'TTJets_TuneCP5_13TeV-madgraphMLM-pythia8':
             extrafolder = beans['TTJets']
             print("Opening", extrafolder)
             print("Looking into", extrafolder+"/"+dataset)
             totalpath = extrafolder+"/"+dataset
-            cmd = "eos {0} find -f {1} | grep 'ffNtuple' > {2}".format("root://cmseos.fnal.gov", totalpath, dataset+"_subdirs.txt")
+            cmd = "eos {0} find -f {1} | grep 'ffNtuple' > {2}".format("root://cmseos.fnal.gov/", totalpath, dataset+"_subdirs.txt")
             os.system(cmd)
             slist=open(dataset+"_subdirs.txt")
             s = slist.readlines()
@@ -94,42 +95,95 @@ for folder in beans['2018']:
                         splits.pop(i)
                     timestamp = splits [-2]
                     timestampdirs.append(timestamp)
-            
-        print("Looking into", folder+"/"+dataset)
-        totalpath = folder+"/"+dataset
-        
-        cmd = "eos {0} find -d {1} | grep '000*/$' > {2}".format("root://cmseos.fnal.gov", totalpath, dataset+"_subdirs.txt")
-        os.system(cmd)
-        slist=open(dataset+"_subdirs.txt")
-        s = slist.readlines()
-        
-        for apath in s:
-            if "/failed/" in apath:
-                continue #filter out failed files
-            if '0000' in apath or '0001' in apath: #ensures folders returned by the grep are at the correct level
-                splits = apath.strip().split('/')
-                for i in range(0, len(splits)):
-                    if splits[i] == '': #fixes an issue where some lists added an empty string at the end after being split
-                        splits.pop(i)
-                timestamp = splits[-2]
-                timestampdirs.append(timestamp)
-                
-        timestampdirs = sorted(
-            timestampdirs, key=lambda x: datetime.strptime(x, "%y%m%d_%H%M%S")
-        )
-        
-        latest = timestampdirs[-1] #selects only most recent timestamp
-        
-        if dataset == 'TTJets_TuneCP5_13TeV-madgraphMLM-pythia8':
-            totalpath = extrafolder+'/'+dataset
-        
-        cmd = "eos {0} find -f --xurl {1} | grep {2} > {3}".format("root://cmseos.fnal.gov/", totalpath, latest, dataset+".txt")
-        os.system(cmd)
-        flist = open(dataset+".txt")
-        f = flist.readlines()
-        urllist = []
-        
-        print('file length:',len(f))
+            timestampdirs = sorted(
+                timestampdirs, key=lambda x: datetime.strptime(x, "%y%m%d_%H%M%S")
+            )
+
+            latest = timestampdirs[-1] #selects only most recent timestamp
+
+            cmd = "eos {0} find -f --xurl {1} | grep {2} > {3}".format("root://cmseos.fnal.gov/", totalpath, latest, dataset+".txt")
+            os.system(cmd)
+            flist = open(dataset+".txt")
+            f = flist.readlines()
+            print('file length:',len(f))
+
+        '''
+        The actual data, DoubleMuon, has four sub-sections: A, B, C, and D. Rather than just the most recent 
+        timestamp, we need the most recent timestamp each for A, B, C, and D, so this section covers that.
+        '''
+
+        elif dataset == 'DoubleMuon':
+            print("Looking into", folder+"/"+dataset)
+            totalpath = folder+"/"+dataset
+            f = []
+
+            cmd = "eos {0} find -d {1} | grep '000*/$' > {2}".format("root://cmseos.fnal.gov/", totalpath, dataset+"_subdirs.txt")
+            os.system(cmd)
+            slist=open(dataset+"_subdirs.txt")
+            s = slist.readlines()
+            for abcd in datadataset.keys():
+                print('looking at: ', abcd)
+                for apath in s:
+                    if "/failed/" in apath:
+                        continue #filter out failed files
+                    if not (abcd in apath):
+                        continue
+                    if '0000' in apath or '0001' in apath: #ensures folders returned by the grep are at the correct level
+                        splits = apath.strip().split('/')
+                        for i in range(0, len(splits)):
+                            if splits[i] == '': #fixes an issue where some lists added an empty string at the end after being split
+                                splits.pop(i)
+                        timestamp = splits[-2]
+                        timestampdirs = []
+                        timestampdirs.append(timestamp)
+
+                timestampdirs = sorted(
+                    timestampdirs, key=lambda x: datetime.strptime(x, "%y%m%d_%H%M%S")
+                )
+
+                latest = timestampdirs[-1] #selects only most recent timestamp
+
+                cmd = "eos {0} find -f --xurl {1} | grep {2} > {3}".format("root://cmseos.fnal.gov/", totalpath, latest,        abcd+'_'+dataset+".txt")
+                os.system(cmd)
+                flist = open(abcd+'_'+dataset+".txt")
+                f += flist.readlines()
+                print('file length:', len(f))
+
+        '''
+        The rest of the datasets function "normally" and we want the most recent timestamp only.
+        '''
+
+        else:
+            print("Looking into", folder+"/"+dataset)
+            totalpath = folder+"/"+dataset
+
+            cmd = "eos {0} find -d {1} | grep '000*/$' > {2}".format("root://cmseos.fnal.gov/", totalpath, dataset+"_subdirs.txt")
+            os.system(cmd)
+            slist=open(dataset+"_subdirs.txt")
+            s = slist.readlines()
+            for apath in s:
+                if "/failed/" in apath:
+                    continue #filter out failed files
+                if '0000' in apath or '0001' in apath: #ensures folders returned by the grep are at the correct level
+                    splits = apath.strip().split('/')
+                    for i in range(0, len(splits)):
+                        if splits[i] == '': #fixes an issue where some lists added an empty string at the end after being split
+                            splits.pop(i)
+                    timestamp = splits[-2]
+                    timestampdirs.append(timestamp)
+
+            timestampdirs = sorted(
+                timestampdirs, key=lambda x: datetime.strptime(x, "%y%m%d_%H%M%S")
+            )
+
+            latest = timestampdirs[-1] #selects only most recent timestamp
+
+            cmd = "eos {0} find -f --xurl {1} | grep {2} > {3}".format("root://cmseos.fnal.gov/", totalpath, latest, dataset+".txt")
+            os.system(cmd)
+            flist = open(dataset+".txt")
+            f = flist.readlines()
+            print('file length:',len(f))
+
         xs = xsections[dataset]
         for path in f:
             s = path.strip().split('/')
@@ -157,6 +211,7 @@ for folder in beans['2018']:
                       }
         os.system("rm "+dataset+".txt")
         os.system("rm "+dataset+"_subdirs.txt")
+        os.system("rm "+abcd+'_'+dataset+".txt")
 
 os.system("mkdir -p beans")
 with open("beans/"+options.year+".json", "w") as fout:
